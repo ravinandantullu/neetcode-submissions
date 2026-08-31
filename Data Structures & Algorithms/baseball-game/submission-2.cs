c4e505e952fbc8b8d3b307735733bdcc741ceab1
Ravinandan Tulluri <Tulluri.ravinandan@gmail.com>
2026-08-31 00:08:33 -0500

    Add: baseball-game - submission-2

---
+++ Data Structures & Algorithms/baseball-game/submission-2.cs
@@ -0,0 +1,38 @@
+public class Solution {
+    public int CalPoints(string[] operations) {
+        Stack<int> numbers = new Stack<int>();
+        for (int i = 0; i < operations.Length; i++) {
+            if (int.TryParse(operations[i], out int result))
+            {
+                numbers.Push(result);
+            }
+            else {
+                if (operations[i] == "+") {
+                    int previousRecordOne = 0;
+                    int previousRecordTwo = 0;
+                    numbers.TryPop(out previousRecordOne);
+                    numbers.TryPop(out previousRecordTwo);
+                    numbers.Push(previousRecordTwo);
+                    numbers.Push(previousRecordOne);
+                    int last = previousRecordOne + previousRecordTwo;
+                    numbers.Push(last);
+                }
+                if (operations[i] == "C") {
+                    numbers.Pop();
+                }
+                if (operations[i] == "D") {
+                    numbers.TryPop(out int previousRecordForD);
+                    numbers.Push(previousRecordForD);
+                    Console.WriteLine(2 * previousRecordForD);
+                    numbers.Push(2 * previousRecordForD);
+                }
+            }
+        }
+        int valuesR = 0;
+        while (numbers.TryPop(out int card))
+        {   
+            valuesR = valuesR + card;
+        }
+        return valuesR;
+    }
+}
